71bd2828239f4b69d4d3786b5f276b70b78928ca
Adams Adeyiga <adamsadeyiga1@gmail.com>
2022-10-27 00:40:11 -0700

    advance tasks corrected

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -11,37 +11,27 @@
 
 void print_buffer(char *b, int size)
 {
-
 	int j, k, l;
 
 	if (size <= 0)
 		printf("\n");
 	else
 	{
-
 		for (j = 0; j < size; j += 10)
 		{
-
 			printf("%.8x:", j);
-
 			for (k = j; k < j + 10; k++)
 			{
-
 				if (k % 2 == 0)
 					printf(" ");
-
 				if (k < size)
 					printf("%.2x", *(b + k));
 				else
 					printf("  ");
-
 			}
-
 			printf(" ");
-
 			for (l = j; l < j + 10; l++)
 			{
-
 				if (l >= size)
 					break;
 
@@ -49,13 +39,8 @@ void print_buffer(char *b, int size)
 					printf("%c", '.');
 				else
 					printf("%c", *(b + l));
-
 			}
-
 			printf("\n");
-
 		}
-
 	}
-
 }
