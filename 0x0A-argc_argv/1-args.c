3eaa23e3d5167f05410640cd0351eb84604a3d75
Adams Adeyiga <adamsadeyiga1@gmail.com>
2023-08-04 07:55:35 +0100

    ADD project 0x0A - agrc argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	// (void) - Implicit
+	printf("%d\n", argc - 1); // Print the number of arguments passed to the program.
+	return (0);
+}
